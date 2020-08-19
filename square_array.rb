140ad0d40ce44b2dc708c832acd078064c9702a1
Aaron Brady <aleebrady@gmail.com>
2020-08-19 19:44:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
+
 def square_array(array)
- squared = []
- array.each do |n|
-   s = n * n 
-   squared << s 
- end
- squared
+  squared = []
+  array.each do |num|
+<<<<<<< HEAD
+    squared_num = num * num
+    squared <<(squared_num)
+=======
+    squared = num * 2
+>>>>>>> b5cfe95b2e9bb26246f7f37ba14c1252025a6d9b
+  end
+  return squared
 end
